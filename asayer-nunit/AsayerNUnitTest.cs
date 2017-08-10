7f742b5f3c7ddc529da44a74f55ea6cee5fef3f3
Kraiem Taha Yassine <tahay.kraiem@acfellows.com>
2017-08-10 20:00:12 +0100

    NUnit code working

---
+++ asayer-nunit/AsayerNUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace asayer_nunit
+{
+    class AsayerNUnitTest
+    {
+        protected IWebDriver driver;
+
+        public AsayerNUnitTest()
+        { }
+
+        [SetUp]
+        public void Init()
+        {
+            NameValueCollection caps = ConfigurationManager.GetSection("capabilities") as NameValueCollection;
+
+            DesiredCapabilities capability = new DesiredCapabilities();
+
+            foreach (string key in caps.AllKeys)
+            {
+                capability.SetCapability(key, caps[key]);
+            }
+
+            string hub = ConfigurationManager.AppSettings.Get("server");
+            string name = ConfigurationManager.AppSettings.Get("name");
+            string apikey = ConfigurationManager.AppSettings.Get("apikey");
+            
+            capability.SetCapability("apikey", apikey);
+            capability.SetCapability("name", name);
+            
+            var timeOut = TimeSpan.FromMinutes(1);
+
+            driver = new RemoteWebDriver(new Uri(hub), capability, timeOut);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+
+        }
+    }
+}
---
+++ asayer-nunit/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace asayer_nunit
+{
+    class Test : AsayerNUnitTest
+    {
+
+        [Test]
+        public void SearchGoogle()
+        {
+            driver.Navigate().GoToUrl("http://www.asayer.io");
+            System.Threading.Thread.Sleep(5000);
+            IWebElement videoButton = driver.FindElement(By.Id("wistia_14.thumb_container"));
+            System.Threading.Thread.Sleep(5000);
+            videoButton.Click();
+            System.Threading.Thread.Sleep(5000);
+            IWebElement video = driver.FindElement(By.Id("wistia_simple_video_34"));
+            Assert.IsTrue(video.Displayed);
+            Assert.AreEqual("QA-as-a-Service | Asayer", driver.Title);
+        }
+    }
+}
